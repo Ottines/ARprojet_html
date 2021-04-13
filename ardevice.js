ed22188b4ea087b66a5ccbab9914c9a20a835a88
Becuwe Ottman <78697425+Ottines@users.noreply.github.com>
2021-04-13 14:20:10 +0200

    2 eme commit polyfill, ajout supportedmodes/features/start

--- ardevice.js
+++ ardevice.js
@@ -1,1 +1,22 @@
-//list string
+let ARDevice = function(deviceConfig) {
+    //let canvas = null; 
+    //let video = null;
+    //let detector = new AR.Detector();
+    
+    
+    Object.defineProperty(this, "started", {
+        get: function() { return started; }
+    });
+    
+    //on force l'appareil de l'utilisateur à supporter l'ar
+    Object.defineProperty(this,"supportedModes", {
+        value : [ 'immersive-ar', 'inline' ],
+        writable: false
+    });
+    
+    Object.defineProperty(this,"enabledFeatures", {
+        value : [ ],
+        writable: false
+    }); 
+}
+
